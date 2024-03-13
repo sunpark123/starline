e02fd31ae07ddc78258fa23e755c8f22ae4c81a9
석현07 <gokpoo31@gmail.com>
2024-03-13 10:28:02 +0900

    rebuild

--- src/main/java/com/starline/starline/StarlineApplication.java
+++ src/main/java/com/starline/starline/StarlineApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.starline.starline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class StarlineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(StarlineApplication.class, args);
 	}
 
 }
--- src/test/java/com/starline/starline/StarlineApplicationTests.java
+++ src/test/java/com/starline/starline/StarlineApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.starline.starline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class StarlineApplicationTests {
 
 	@Test
 	void contextLoads() {
